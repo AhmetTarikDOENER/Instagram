500a06a6687203fd09412e2c0245ee1a5077c6f2
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2023-10-30 22:44:23 +0300

    Added manager files and started to build login screen components

---
+++ Instagram/Managers/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  Instagram
+//
+//  Created by Ahmet Tarik DÖNER on 30.10.2023.
+//
+
+import Foundation
---
+++ Instagram/Managers/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  Instagram
+//
+//  Created by Ahmet Tarik DÖNER on 30.10.2023.
+//
+
+import Foundation
