3f67be9537567f69e5c0b0d4f434a81d242599c2
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2023-10-31 18:58:57 +0300

    Created blueprint of collection view that used in profile screen.

--- Instagram/Controllers/Core Tabs/ProfileViewController.swift
+++ Instagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -7,17 +7,45 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+/// Profile view controller
+final class ProfileViewController: UIViewController {
 
+    private var collectionView: UICollectionView?
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureNavigationBar()
+        setCollectionViewDelegateDatasource()
+        configureCollectionView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
     }
     
     //MARK: - Private
+    private func configureCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: view.width / 3, height: view.width / 3)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        view.addSubview(collectionView)
+    }
+    
+    private func setCollectionViewDelegateDatasource() {
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+    }
     
     private func configureNavigationBar() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettingsButton))
@@ -28,4 +56,22 @@ class ProfileViewController: UIViewController {
         vc.title = "Settings"
         navigationController?.pushViewController(vc, animated: true)
     }
+}
+//MARK: - UICollectionViewDelegate and UICollectionViewDatasource
+
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+    }
+    
 }
