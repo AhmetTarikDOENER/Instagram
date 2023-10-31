65d00711263cf2dd51a690a015e2f48a0deb9843
Ahmet Tarik DÖNER <tarik.iosdev@icloud.com>
2023-10-31 19:19:42 +0300

    Created simple main components of other screens.

--- Instagram/Controllers/Core Tabs/NotificationsViewController.swift
+++ Instagram/Controllers/Core Tabs/NotificationsViewController.swift
@@ -9,8 +9,44 @@ import UIKit
 
 class NotificationsViewController: UIViewController {
 
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        title = "Notifications"
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        setTableViewDelegateDatasource()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    //MARK: - Private
+    private func setTableViewDelegateDatasource() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension NotificationsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Deneme"
+        return cell
     }
 }
